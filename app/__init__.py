508cf0fa03aa92e0cf7d55d64ea2f3970e63d738
diarmaid <diarmaiddeburca@gmail.com>
2021-03-06 20:57:01 +0100

    Miley reactions

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,13 @@ from app.sayings import Sayings
 from app.protec_attac import ProtecAttac
 from app.Entertainment import Entertainment
 from app.statistisc import Information
+from app.MileyReaction import MileyReaction
 
 bot.add_cog(Information(bot))
 bot.add_cog(Entertainment(bot))
 bot.add_cog(ProtecAttac(bot))
 bot.add_cog(Sayings(bot))
+bot.add_cog(MileyReaction(bot))
 
 
 
